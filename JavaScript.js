a783a8099bc5da400f31e2167285aa96136dc394
Brendan Hafele <brendan.hafele@gmail.com>
2017-11-28 14:56:47 -0800

    Small update
    added small html file and js file

---
+++ JavaScript.js
@@ -0,0 +1,14 @@
+﻿const heading = document.getElementById("heading");
+const btnTextColor = document.getElementById("btnTextColor");
+const btnBGColor = document.getElementById("btnBGColor");
+const colorCode = document.getElementById("colorCode");
+const colorCodeBG = document.getElementById("colorCodeBG");
+
+
+btnTextColor.addEventListener("click", () => {
+    heading.style.color = colorCode.value;
+});
+
+btnBGColor.addEventListener("click", () => {
+    heading.style.backgroundColor = colorCodeBG.value;
+});
